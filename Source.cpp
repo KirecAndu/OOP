dad8bcbb591dcf509850617c42b04234d9eeb068
ИГОРЬ <>
2016-09-11 22:05:39 +0100

    second

--- Source.cpp
+++ Source.cpp
@@ -4,16 +4,22 @@
 using namespace std;
 
 class oop1{
-public: char a[20];
+public: char a[20]; 
+		int b;
+
 		void hitler() {
 			cout << "Where are you from?" << endl;
 			cin >> a;
 			cout << "You live in " << a << endl;
 		}
+		void adolf() {
+			cout << "8 * 8 = " << 8*8 <<endl;
+		}
 };
 
 void main() {
 	oop1 apple;
+	apple.adolf();
 	apple.hitler();
 	system("pause");
 }
